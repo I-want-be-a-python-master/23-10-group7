86fa9812131336ea1c75d25fe5102dbf989e799c
humans-job <zhanghengjia2004@gmail.com>
2024-06-30 18:56:29 +0800

    Delete 27-homework directory

--- 27-homework/27.homework.2.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-data = pd.read_csv("data_standardization.csv")
-data['Price'] = data['Price'].str.replace('[$, USD]', '', regex=True).astype(float)
-data = data.replace('None',0)
-data['Total_Value']  =data.Price*data.Quantity
-data.head(10)
-data[['Category','Price']].groupby(by='Category').mean()
-data[['Total_Value','Category']].groupby(by='Category').sum()
